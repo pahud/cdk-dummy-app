caa4c3afd801efa4d05d7b6c52352eb2aaac88bf
Pahud Hsieh <pahudnet@gmail.com>
2024-01-25 17:46:57 +0000

    chore: project created with projen

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import {
+  App, Stack, StackProps,
+  aws_sqs as sqs,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    new sqs.Queue(this, 'DummyQueue');
+
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'cdk-dummy-app-dev', { env: devEnv });
+// new MyStack(app, 'cdk-dummy-app-prod', { env: prodEnv });
+
+app.synth();
